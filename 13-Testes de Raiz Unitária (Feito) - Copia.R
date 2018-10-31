cd26342a58559edeecafbf227984b107ed3c4235
Flavia-Albieri <42556998+Flavia-Albieri@users.noreply.github.com>
2018-10-31 20:46:40 -0300

    Add files via upload

--- 13-Testes de Raiz Unitária (Feito) - Copia.R
+++ 13-Testes de Raiz Unitária (Feito) - Copia.R
@@ -1,14 +1,14 @@
                           #Aula 13 - Testes de Raiz Unitária
-install.packages("forecast")                 #Instala Pacote Forecast
-install.packages(urca)
-install.packages("tseries")
-install.packages(readxl)
-install.packages("pwt8")
-library(forecast)
-library(tseries)
-library("urca")                                #Carrega Pacote URCA
-library(readxl)                                #Carrega Pacote readxl
-library(pwt8)                                  #Carrega o pacote PWT8.0
+install.packages("forecast")                 #Instala pacote forecast
+install.packages(urca)                       #Instala pacote urca
+install.packages("tseries")                  #Instala pacote tseries
+install.packages(readxl)                     #Instala pacote readxl
+install.packages("pwt8")                     #Instala pacote pwt8
+library(forecast)                            #Carrega o pacote forecast
+library(tseries)                             #Carrega o pacote tseries
+library("urca")                              #Carrega o pacote urca
+library(readxl)                              #Carrega o pacote readxl
+library(pwt8)                                #Carrega o pacote pwt8
 
 
 data("pwt8.0")                                 #Carrega os dados elencados "pwt8.0" dispoiníveis no pacote
@@ -27,31 +27,31 @@ CAMBIO <- br$Câmbio[45:62]              #Cria o vetor para variável CAMBIO
 Anos <- seq(from=1994, to=2011, by=1)   #Cria um vetor para o tempo em anos de 1994 até 2011 
 
 
-                                    #Analise para o PIB
+                                    #Análise para o PIB
 
 plot(PIB, type = "l")                            #Cria gráfico para o PIB
-pib <- ts(PIB, start = 1994, frequency = 1)  #Define como Série Temporal
+pib <- ts(PIB, start = 1994, frequency = 1)      #Define como Série Temporal
 plot(pib, main="Produto Interno Bruto", 
-     ylab="Milhoes de dolares", 
-     xlab="Ano")                                      #Cria gráfico da Série Temporal
+     ylab="Milhões de dólares", 
+     xlab="Ano")                                 #Cria gráfico da Série Temporal
 
-acf(pib)                                          #Função de Autocorrelação
-pacf(pib)                                         ##Função de Autocorrelação Parcial
-reglinPIB <- lm(PIB ~ Anos)                       #Regressão linear simples do emprego em relação ao tempo
-reglinPIB                                         #Exibe os resultados da regressão linear
+acf(pib)                                         #Função de Autocorrelação
+pacf(pib)                                        ##Função de Autocorrelação Parcial
+reglinPIB <- lm(PIB ~ Anos)                      #Regressão linear simples do emprego em relação ao tempo
+reglinPIB                                        #Exibe os resultados da regressão linear
 summary(reglinPIB)
-plot(pib)                                         #Gráfcio dos dados
-abline(reglinPIB, col="Blue")                     #Insere a linha de regressão linear estimada
+plot(pib)                                        #Gráfcio dos dados
+abline(reglinPIB, col="Blue")                    #Insere a linha de regressão linear estimada
 
 #Teste ADF
 
 TesteADF_PIB_trend <- ur.df(pib, "trend", lags = 1)         #Teste DF-DickFuller com drift e com tendencia
 summary(TesteADF_PIB_trend) 
 
-TesteADF_PIB_drift <- ur.df(pib, "drif", lags = 1)         #Teste DF-DickFuller com drift e com tendencia
+TesteADF_PIB_drift <- ur.df(pib, "drift", lags = 1)         #Teste DF-DickFuller com drift e com tendencia
 summary(TesteADF_PIB_drift) 
 
-TesteADF_PIB_none <- ur.df(pib, "none", lags = 1)         #Teste DF-DickFuller com drift e com tendencia
+TesteADF_PIB_none <- ur.df(pib, "none", lags = 1)           #Teste DF-DickFuller com drift e com tendencia
 summary(TesteADF_PIB_none)
 
 #Teste Phillips-Perron
@@ -62,11 +62,11 @@ pp.test(pib)
 
 kpss.test(pib)
 
-#Removendo Tendência por Meio de Regressão Linear
+#Removendo Tendência por meio de Regressão Linear
 
 residuosPIB <- reglinPIB$residuals                    #Salva os resíduos no vetor residuosEMP
 reglinPIBres <- lm(residuosPIB ~ Anos)                #Regressão linear dos resíduos em função do tempo
-plot(residuosPIB,type="l")                            #Gráfico dos resíduos
+plot(residuosPIB, type = "l")                         #Gráfico dos resíduos
 abline(reglinPIBres, col="Blue")                      #Insere a linha de regressão linear dos resíduos
 acf(residuosPIB)
 
@@ -93,9 +93,9 @@ kpss.test(residuosPIB)
 
 pdPIB <- diff(PIB)                                               #Calcula a primeira diferença da série de dados
 diferenca1PIB <- (data.frame(PIB[2:18],pdPIB))                   #Exibe a tabela da série original coma diferença <- 
-DIFERENCAPIB <- ts(diferenca1PIB, start = 1994, frequency = 1)   #Define serie temporal para a tabela diferenca1
+DIFERENCAPIB <- ts(diferenca1PIB, start = 1994, frequency = 1)   #Define série temporal para a tabela diferenca1
 plot(DIFERENCAPIB, plot.type="single", col=c("Black","Green"))   #Cria o grafico com as duas series
-plot(pdePIB, type="l")                                           #Cria gráfico somente para a serie da diferença
+plot(pdPIB, type="l")                                            #Cria gráfico somente para a serie da diferença
 
 #Teste ADF
 
@@ -128,32 +128,26 @@ arima110 <- arima(pib, c(1,1,0))
 arima111 <- arima(pib, c(1,1,1))
 arima112 <- arima(pib, c(1,1,2))
 
-arima210 <-  arima(pib, c(2,1,0))
-arima211 <-  arima(pib, c(2,1,1))
-arima212 <-  arima(pib, c(2,1,2))
-arima213 <-  arima(pib, c(2,1,3))
 #MA
-arima011 <-  arima(pib, c(0,1,1))
-arima012 <-  arima(pib, c(0,1,2))
-arima013 <-  arima(pib, c(0,1,3))
+arima011 <- arima(pib, c(0,1,1))
+arima012 <- arima(pib, c(0,1,2))
+arima013 <- arima(pib, c(0,1,3))
 #AR
-arima020 <-  arima(pib, c(0,1,0))
+arima010 <- arima(pib, c(0,1,0))
 
 #Escolher o melhor modelo com base no menor AIC/BIC
 estimacoes <- list(arima113,arima110,arima111,
-                   arima112,arima210,arima211,
-                   arima212,arima213,arima011,arima011, arima012,
+                   arima112,arima011, arima012,
                    arima013,arima010)
 AIC <- sapply(estimacoes, AIC)
 AIC <- as.data.frame(AIC)
 BIC <- sapply(estimacoes, BIC)
 BIC <- as.data.frame(BIC)
 Modelo <-c("arima113","arima110","arima111",
-                "arima112","arima210","arima211",
-                "arima212","arima213","arima011","arima011", "arima012",
+                "arima112","arima011", "arima012",
                 "arima013","arima010") 
 Resultados <- data.frame(Modelo,AIC,BIC)
-View(Resultados)
+View(Resultados) #parei aqui
 
 #Previsão - Notas na Aula 11
 
